b1273467a7ce42f103aa69961dbcff59bf258787
rodrigoyr <ryeperub@myuax.com>
2024-04-04 20:21:01 +0200

    commit

--- Desarrollo de un Sistema de Gestion de Publicaciones/src/main/java/interfaz/VentanaPrincipal.java
+++ Desarrollo de un Sistema de Gestion de Publicaciones/src/main/java/interfaz/VentanaPrincipal.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class VentanaPrincipal extends JFrame {
     private JMenuItem menuItemNuevo;
     private JMenuItem menuItemAbrir;
     private JMenuItem menuItemGuardar;
     private JMenuItem menuItemSalir;
+    private JDesktopPane desktopPane;
 
     public VentanaPrincipal() {
         super("Sistema de Gestión de Publicaciones");
@@ -21,6 +23,12 @@ public class VentanaPrincipal extends JFrame {
         menuItemGuardar = new JMenuItem("Guardar Documento");
         menuItemSalir = new JMenuItem("Salir");
 
+        menuItemGuardar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                guardarDocumento();
+            }
+        });
+
         menuArchivo.add(menuItemNuevo);
         menuArchivo.add(menuItemAbrir);
         menuArchivo.add(menuItemGuardar);
@@ -30,6 +38,9 @@ public class VentanaPrincipal extends JFrame {
         menuBar.add(menuArchivo);
 
         setJMenuBar(menuBar);
+
+        desktopPane = new JDesktopPane();
+        setContentPane(desktopPane);
     }
 
     public JMenuItem getMenuItemNuevo() {
@@ -46,6 +57,20 @@ public class VentanaPrincipal extends JFrame {
 
     public JMenuItem getMenuItemSalir() {
         return menuItemSalir;
+    }
+
+    private void guardarDocumento() {
+        JInternalFrame[] frames = desktopPane.getAllFrames();
+        for (JInternalFrame frame : frames) {
+            if (frame instanceof VentanaDocumento) {
+                VentanaDocumento ventanaDocumento = (VentanaDocumento) frame;
+                ventanaDocumento.guardarDocumento();
+            }
+        }
+    }
+
+    public JDesktopPane getDesktopPane() {
+        return desktopPane;
     }
 
     public static void main(String[] args) {
