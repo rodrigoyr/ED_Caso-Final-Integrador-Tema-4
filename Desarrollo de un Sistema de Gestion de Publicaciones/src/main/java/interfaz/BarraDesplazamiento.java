9bee2771174b11f59948df01a975536dde7c8d36
rodrigoyr <ryeperub@myuax.com>
2024-04-04 20:03:32 +0200

    commit

--- Desarrollo de un Sistema de Gestion de Publicaciones/src/main/java/interfaz/BarraDesplazamiento.java
+++ Desarrollo de un Sistema de Gestion de Publicaciones/src/main/java/interfaz/BarraDesplazamiento.java
@@ -1,4 +1,39 @@
-package interfaz;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.event.*;
 
-public class BarraDesplazamiento {
+public class BarraDesplazamiento extends JFrame {
+    private JTextArea areaTexto;
+    private JScrollBar barraDesplazamiento;
+
+    public BarraDesplazamiento() {
+        super("Barra de Desplazamiento Interactiva");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+
+        areaTexto = new JTextArea();
+        areaTexto.setLineWrap(true);
+
+        barraDesplazamiento = new JScrollBar(JScrollBar.VERTICAL, 0, 1, 0, 101);
+        barraDesplazamiento.addAdjustmentListener(new AdjustmentListener() {
+            public void adjustmentValueChanged(AdjustmentEvent e) {
+                areaTexto.setCaretPosition((int)(areaTexto.getDocument().getLength() * e.getValue() / 100.0));
+            }
+        });
+
+        JScrollPane scrollPane = new JScrollPane(areaTexto);
+        scrollPane.setVerticalScrollBar(barraDesplazamiento);
+
+        add(scrollPane, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new BarraDesplazamiento().setVisible(true);
+            }
+        });
+    }
 }
