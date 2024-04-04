37dac1c19a1f7517ff3f9b186c0b26c5f8a5106e
rodrigoyr <ryeperub@myuax.com>
2024-04-04 19:57:34 +0200

    commit

--- Desarrollo de un Sistema de Gestion de Publicaciones/src/main/java/editor/NavegacionListado.java
+++ Desarrollo de un Sistema de Gestion de Publicaciones/src/main/java/editor/NavegacionListado.java
@@ -1,8 +1,8 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.*;
 import java.io.*;
-import java.util.ArrayList;
 
 public class NavegacionListado extends JFrame {
     private JList<String> listaDocumentos;
