dca51edb600d533d07d931423204495abf48bbc7
rodrigoyr <ryeperub@myuax.com>
2024-04-04 20:10:26 +0200

    commit

--- Desarrollo de un Sistema de Gestion de Publicaciones/src/main/java/interfaz/VentanaDocumento.java
+++ Desarrollo de un Sistema de Gestion de Publicaciones/src/main/java/interfaz/VentanaDocumento.java
@@ -1,4 +1,41 @@
-package interfaz;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
 
-public class VentanaDocumento {
+public class VentanaDocumento extends JFrame {
+    private JTextArea areaTexto;
+
+    public VentanaDocumento(String nombreDocumento) {
+        super(nombreDocumento);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+
+        areaTexto = new JTextArea();
+        areaTexto.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(areaTexto);
+        add(scrollPane, BorderLayout.CENTER);
+
+        cargarDocumento(nombreDocumento);
+    }
+
+    private void cargarDocumento(String nombreDocumento) {
+        try (BufferedReader lector = new BufferedReader(new FileReader(nombreDocumento))) {
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                areaTexto.append(linea + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaDocumento("documentos/documento.txt").setVisible(true);
+            }
+        });
+    }
 }
